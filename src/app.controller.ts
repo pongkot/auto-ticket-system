9b170fdee49d4b505fea34b9f5b80f890db99ad2
Pongkot Sae-li <pongkot.saeli@outlook.com>
2021-03-31 21:16:43 +0700

    snap: create parking, park and leave

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Observable } from 'rxjs';
+import { map, mergeAll, mergeMap, toArray } from 'rxjs/operators';
+import * as _ from 'lodash';
 
 @Controller()
 export class AppController {
@@ -8,5 +11,40 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('create-parking-lot')
+  createParkingLot(): Observable<any> {
+    return this.appService.createPackingLotStage();
+  }
+
+  @Get('create-ticket')
+  createTicket(@Query('plate') licencePlate: string): Observable<any> {
+    const gateAddress = {
+      x: 0,
+      y: 0,
+    };
+    return this.appService.listPackingLotAvaliable().pipe(
+      mergeAll(),
+      mergeMap((slot: any) =>
+        this.appService.getDistanceFromGate(slot.slotAddress, gateAddress).pipe(
+          map((distance) => {
+            slot['distance'] = distance;
+            return slot;
+          }),
+        ),
+      ),
+      toArray(),
+      map((docs) => {
+        const list = _.sortBy(docs, 'distance');
+        return list[0];
+      }),
+      mergeMap((doc) => this.appService.createTicket(licencePlate, doc)),
+    );
+  }
+
+  @Get('leave-ticket')
+  leaveTicket(@Query('plate') licencePlate: string): Observable<any> {
+    return this.appService.leaveTicket(licencePlate);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,105 @@
 import { Injectable } from '@nestjs/common';
+import { Mongo } from './Mongo';
+import { from, Observable, of } from 'rxjs';
+import { mergeMap, toArray } from 'rxjs/operators';
+import { ObjectId } from 'mongodb';
 
 @Injectable()
-export class AppService {
+export class AppService extends Mongo<any> {
+  constructor() {
+    super();
+    this.setDatabaseOption({
+      database: 'auto_ticket_system',
+      host: 'localhost',
+      port: 27017,
+    });
+  }
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  createPackingLotStage(): Observable<any> {
+    const slotList = [
+      {
+        slotId: 'l-1',
+        slotAddress: {
+          x: 2,
+          y: 0,
+        },
+        available: true,
+        assign: null,
+      },
+      {
+        slotId: 'l-2',
+        slotAddress: {
+          x: 3,
+          y: 0,
+        },
+        available: true,
+        assign: null,
+      },
+      {
+        slotId: 'l-3',
+        slotAddress: {
+          x: 1,
+          y: 0,
+        },
+        available: true,
+        assign: null,
+      },
+    ];
+    return from(slotList).pipe(
+      mergeMap((slot) => this.collection('parkingLotStage').insertOne(slot)),
+    );
+  }
+
+  listPackingLotAvaliable(): Observable<any> {
+    const cursor = this.collection('parkingLotStage').find({ available: true });
+    return from(cursor);
+  }
+
+  getDistanceFromGate(
+    address: { x: number; y: number },
+    gateAddress: { x: number; y: number },
+  ): Observable<number> {
+    const distance = Math.sqrt(
+      Math.pow(address.x - gateAddress.x, 2) -
+        Math.pow(address.y - gateAddress.y, 2),
+    );
+    return of(distance);
+  }
+
+  createTicket(licencePlate: string, slot: any): Observable<any> {
+    const _id = new ObjectId(slot._id);
+    return of(_id).pipe(
+      mergeMap((id) =>
+        this.collection('parkingLotStage').updateOne(
+          { _id: id },
+          {
+            $set: {
+              available: false,
+              assign: licencePlate,
+            },
+          },
+        ),
+      ),
+    );
+  }
+
+  leaveTicket(licencePlate: string): Observable<any> {
+    return of(licencePlate).pipe(
+      mergeMap((plate) =>
+        this.collection('parkingLotStage').updateOne(
+          { assign: plate },
+          {
+            $set: {
+              available: true,
+              assign: null,
+            },
+          },
+        ),
+      ),
+    );
   }
 }
