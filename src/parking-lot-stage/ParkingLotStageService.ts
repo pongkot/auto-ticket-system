729166bba7eb3c6211faf8408fae41589dce0715
Pongkot Sae-li <pongkot.saeli@outlook.com>
2021-04-01 11:23:47 +0700

    implement simple create parking lot

--- src/parking-lot-stage/ParkingLotStageService.ts
+++ src/parking-lot-stage/ParkingLotStageService.ts
@@ -1,10 +1,11 @@
 import { IParkingLotStageService } from './interfaces/IParkingLotStageService';
 import { Inject, Injectable, Logger } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
 import { CONFIG, Mapping, Repository } from '../constants';
 import { IConfig } from '../common/interfaces';
 import { IParkingLotStageRepository } from './interfaces/IParkingLotStageRepository';
 import { ParkingLotStageMapping } from './ParkingLotStageMapping';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class ParkingLotStageService implements IParkingLotStageService {
@@ -20,6 +21,13 @@ export class ParkingLotStageService implements IParkingLotStageService {
   ) {}
 
   createParkingLot(size: 3 | 4): Observable<{ parkingLotId: Array<string> }> {
-    return undefined;
+    const { parkingLotSize } = this.config;
+    const parkingLotList = parkingLotSize[size];
+    return from(parkingLotList).pipe(
+      tap((e) => console.log(e)),
+      map(() => {
+        return { parkingLotId: [] };
+      }),
+    );
   }
 }
