cdf170b19ebeb36c62503db7fb59efdf0020a5b0
Pongkot Sae-li <pongkot.saeli@outlook.com>
2021-04-01 10:27:32 +0700

    add parking area config

---
+++ htdocs/parking-config.ts
@@ -0,0 +1,89 @@
+export const parking1x3 = [
+  {
+    slotId: 'A-1',
+    slotAddress: {
+      lat: 0,
+      long: 1,
+    },
+    available: true,
+    assign: {
+      licencePlate: '',
+      size: '',
+    },
+  },
+  {
+    slotId: 'A-2',
+    slotAddress: {
+      lat: 0,
+      long: 2,
+    },
+    available: true,
+    assign: {
+      licencePlate: '',
+      size: '',
+    },
+  },
+  {
+    slotId: 'A-3',
+    slotAddress: {
+      lat: 0,
+      long: 3,
+    },
+    available: true,
+    assign: {
+      licencePlate: '',
+      size: '',
+    },
+  },
+];
+
+export const parking1x4 = [
+  {
+    slotId: 'A-1',
+    slotAddress: {
+      lat: 0,
+      long: 1,
+    },
+    available: true,
+    assign: {
+      licencePlate: '',
+      size: '',
+    },
+  },
+  {
+    slotId: 'A-2',
+    slotAddress: {
+      lat: 0,
+      long: 2,
+    },
+    available: true,
+    assign: {
+      licencePlate: '',
+      size: '',
+    },
+  },
+  {
+    slotId: 'A-3',
+    slotAddress: {
+      lat: 0,
+      long: 3,
+    },
+    available: true,
+    assign: {
+      licencePlate: '',
+      size: '',
+    },
+  },
+  {
+    slotId: 'A-4',
+    slotAddress: {
+      lat: 0,
+      long: 4,
+    },
+    available: true,
+    assign: {
+      licencePlate: '',
+      size: '',
+    },
+  },
+];
