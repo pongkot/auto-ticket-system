37cc77181339814ca61139f027eb57c288197c4a
Pongkot Sae-li <pongkot.saeli@outlook.com>
2021-04-01 10:40:43 +0700

    create repository

---
+++ src/parking-lot-stage/ParkingLotStageRepository.ts
@@ -0,0 +1,29 @@
+import { Mongo } from '../common';
+import { IParkingLotStageRepository } from './interfaces/IParkingLotStageRepository';
+import { IParkingLotStageSchema } from '../../htdocs/database/auto-ticket-system';
+import { Observable } from 'rxjs';
+import { InsertWriteOpResult } from 'mongodb';
+import { Inject, Logger } from '@nestjs/common';
+import { CONFIG } from '../constants';
+import { IConfig } from '../common/interfaces';
+
+export class ParkingLotStageRepository
+  extends Mongo<IParkingLotStageSchema>
+  implements IParkingLotStageRepository {
+  private readonly logger: Logger = new Logger('ParkingLotStageRepository');
+
+  constructor(
+    @Inject(CONFIG)
+    private readonly config: IConfig,
+  ) {
+    super();
+    const { autoTicketSystem } = this.config.database;
+    this.setDatabaseOption(autoTicketSystem);
+  }
+
+  createParkingLotStage(
+    doc: IParkingLotStageSchema,
+  ): Observable<InsertWriteOpResult<{ _id: string }>> {
+    return undefined;
+  }
+}
