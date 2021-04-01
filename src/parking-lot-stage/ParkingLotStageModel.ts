a26687a7ac44e1577b9d850b121c76eb802a3476
Pongkot Sae-li <pongkot.saeli@outlook.com>
2021-04-01 10:51:54 +0700

    add parking lot stage model

---
+++ src/parking-lot-stage/ParkingLotStageModel.ts
@@ -0,0 +1,72 @@
+export class ParkingLotStageModel {
+  private slotId: string;
+  private slotAddressLat: number;
+  private slotAddressLong: number;
+  private available: boolean;
+  private ticketId: string;
+  private licencePlate: string;
+  private size: string;
+
+  getSlotId(): string {
+    return this.slotId;
+  }
+
+  setSlotId(slotId: string): this {
+    this.slotId = slotId;
+    return this;
+  }
+
+  getSlotAddressLat(): number {
+    return this.slotAddressLat;
+  }
+
+  setSlotAddressLat(slotAddressLat: number): this {
+    this.slotAddressLat = slotAddressLat;
+    return this;
+  }
+
+  getSlotAddressLong(): number {
+    return this.slotAddressLong;
+  }
+
+  setSlotAddressLong(slotAddressLong: number): this {
+    this.slotAddressLong = slotAddressLong;
+    return this;
+  }
+
+  getAvailable(): boolean {
+    return this.available;
+  }
+
+  setAvailable(available: boolean): this {
+    this.available = available;
+    return this;
+  }
+
+  getTicketId(): string {
+    return this.ticketId;
+  }
+
+  setTicketId(ticketId: string): this {
+    this.ticketId = ticketId;
+    return this;
+  }
+
+  getLicencePlate(): string {
+    return this.licencePlate;
+  }
+
+  setLicencePlate(licencePlate: string): this {
+    this.licencePlate = licencePlate;
+    return this;
+  }
+
+  getSize(): string {
+    return this.size;
+  }
+
+  setSize(size: string): this {
+    this.size = size;
+    return this;
+  }
+}
