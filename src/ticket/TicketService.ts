b59c18350a3b7447c3bba1e77acb3f3a92a73a00
Pongkot Sae-li <pongkot.saeli@outlook.com>
2021-04-02 09:58:05 +0700

    add validate duplicate licence plate
    Signed-off-by: Pongkot Sae-li <pongkot.saeli@outlook.com>

--- src/ticket/TicketService.ts
+++ src/ticket/TicketService.ts
@@ -9,10 +9,11 @@ import {
 import { from, Observable } from 'rxjs';
 import { Repository, Service } from '../constants';
 import { IParkingLotStageService } from '../parking-lot-stage/interfaces/IParkingLotStageService';
-import { map, mergeMap, toArray } from 'rxjs/operators';
+import { map, mergeMap, tap, toArray } from 'rxjs/operators';
 import { ParkingLotStageModel } from '../parking-lot-stage/ParkingLotStageModel';
 import { IParkingLotStageRepository } from '../parking-lot-stage/interfaces/IParkingLotStageRepository';
 import { UpdateWriteOpResult } from 'mongodb';
+import * as _ from 'lodash';
 
 @Injectable()
 export class TicketService implements ITicketService {
@@ -24,50 +25,104 @@ export class TicketService implements ITicketService {
     private readonly parkingLotStageRepository: IParkingLotStageRepository,
   ) {}
 
+  // TODO refactor
   createTicket(
     licencePlate: string,
     carSize: 's' | 'm' | 'l',
-  ): Observable<{ ticketId: string; yourSlot: Array<string> }> {
-    return this.parkingLotStageService.listAvailableParkingLot().pipe(
-      toArray(),
-      mergeMap((slotList: Array<ParkingLotStageModel>) =>
-        this.parkingLotStageService.observeSlotForCarSize(slotList, carSize),
-      ),
-      mergeMap((slot: { available: number }) => {
-        if (slot.available <= 0) {
-          throw new HttpException(
-            'No available parking lot',
-            HttpStatus.NOT_FOUND,
+  ): Observable<any> {
+    return this.parkingLotStageRepository
+      .searchParkingLotStage({ 'assign.licencePlate': licencePlate })
+      .pipe(
+        toArray(),
+        tap((e) => {
+          if (_.size(e) >= 1) {
+            throw new HttpException(
+              'Licence plate has been used',
+              HttpStatus.BAD_REQUEST,
+            );
+          }
+        }),
+        mergeMap(() => this.parkingLotStageService.listAvailableParkingLot()),
+        toArray(),
+        mergeMap((slotList: Array<ParkingLotStageModel>) =>
+          this.parkingLotStageService.observeSlotForCarSize(slotList, carSize),
+        ),
+        mergeMap((slot: { available: number }) => {
+          if (slot.available <= 0) {
+            throw new HttpException(
+              'No available parking lot',
+              HttpStatus.NOT_FOUND,
+            );
+          }
+          return this.parkingLotStageService.rangingAvailableAndShortDistanceSlot();
+        }),
+        mergeMap((availableSlotList: Array<ParkingLotStageModel>) => {
+          const carDoc = {
+            licencePlate,
+            carSize,
+          };
+          return this.parkingLotStageService.parkingActivate(
+            carDoc,
+            availableSlotList,
           );
-        }
-        return this.parkingLotStageService.rangingAvailableAndShortDistanceSlot();
-      }),
-      mergeMap((availableSlotList: Array<ParkingLotStageModel>) => {
-        const carDoc = {
-          licencePlate,
-          carSize,
-        };
-        return this.parkingLotStageService.parkingActivate(
-          carDoc,
-          availableSlotList,
-        );
-      }),
-      mergeMap((ticket: { ticketId: string }) =>
-        this.parkingLotStageService
-          .searchParkingSlotByTicketId(ticket.ticketId)
-          .pipe(
-            toArray(),
-            map((slotsList: Array<ParkingLotStageModel>) => {
-              return {
-                ticketId: ticket.ticketId,
-                yourSlot: slotsList.map((Doc: ParkingLotStageModel) =>
-                  Doc.getSlotId(),
-                ),
-              };
-            }),
-          ),
-      ),
-    );
+        }),
+        mergeMap((ticket: { ticketId: string }) =>
+          this.parkingLotStageService
+            .searchParkingSlotByTicketId(ticket.ticketId)
+            .pipe(
+              toArray(),
+              map((slotsList: Array<ParkingLotStageModel>) => {
+                return {
+                  ticketId: ticket.ticketId,
+                  yourSlot: slotsList.map((Doc: ParkingLotStageModel) =>
+                    Doc.getSlotId(),
+                  ),
+                };
+              }),
+            ),
+        ),
+      );
+
+    // return this.parkingLotStageService.listAvailableParkingLot().pipe(
+    //   toArray(),
+    //   mergeMap((slotList: Array<ParkingLotStageModel>) =>
+    //     this.parkingLotStageService.observeSlotForCarSize(slotList, carSize),
+    //   ),
+    //   mergeMap((slot: { available: number }) => {
+    //     if (slot.available <= 0) {
+    //       throw new HttpException(
+    //         'No available parking lot',
+    //         HttpStatus.NOT_FOUND,
+    //       );
+    //     }
+    //     return this.parkingLotStageService.rangingAvailableAndShortDistanceSlot();
+    //   }),
+    //   mergeMap((availableSlotList: Array<ParkingLotStageModel>) => {
+    //     const carDoc = {
+    //       licencePlate,
+    //       carSize,
+    //     };
+    //     return this.parkingLotStageService.parkingActivate(
+    //       carDoc,
+    //       availableSlotList,
+    //     );
+    //   }),
+    //   mergeMap((ticket: { ticketId: string }) =>
+    //     this.parkingLotStageService
+    //       .searchParkingSlotByTicketId(ticket.ticketId)
+    //       .pipe(
+    //         toArray(),
+    //         map((slotsList: Array<ParkingLotStageModel>) => {
+    //           return {
+    //             ticketId: ticket.ticketId,
+    //             yourSlot: slotsList.map((Doc: ParkingLotStageModel) =>
+    //               Doc.getSlotId(),
+    //             ),
+    //           };
+    //         }),
+    //       ),
+    //   ),
+    // );
   }
 
   leaveTicket(ticketId: string): Observable<{ message: string }> {
